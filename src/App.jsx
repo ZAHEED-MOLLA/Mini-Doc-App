333597e45da5bcba9e978a77009a36bd395b0c52
ZAHEED-MOLLA <zaheedhossain06@gmail.com>
2024-04-04 21:34:35 +0530

    Using Framer motion

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Background from './components/Background';
+import ForGround from './components/ForGround';
+
+const App = () => {
+  return (
+    <div className='relative w-full h-screen bg-zinc-800'>
+     <Background />
+     <ForGround />
+    </div>
+  );
+}
+
+export default App;
+
